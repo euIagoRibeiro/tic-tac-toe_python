6c922d4ed166621877daa90179e0dfed57b73100
euIagoRibeiro <iagoribeirospider2007dpr@gmail.com>
2025-08-24 23:01:56 -0300

    feat: adiciona uma matriz e uma função que possibilita sua visualização

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-# o que um jogo da velha precisa ter
-# uma matriz(3x3) que irar formar o jogo em si
-# uma forma do jogador poder escolher o lugar da matriz que quer colcar seu "valor"
-# uma lógica de turnos, onde ira aderir o respectivo "valor" do jogar
-## se o turno for par será "X" se for impar sera "Y"
-# talvez uma lógica de pontos, atribuindo a ideia de classe e objetos
-## junto com isso, pode existir um menu
+
+quadro = [
+    ['-','-','-'],
+    ['-','-','-'],
+    ['-','-','-']
+]
+
+def exibir_jogo():
+    for linha in quadro:
+        for elemento in linha:
+            print(f'| {elemento} |', end='')
+        print()
+
+exibir_jogo()
