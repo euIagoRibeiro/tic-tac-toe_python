727378f2cc8cab1d178307ae33b2aed160143973
euIagoRibeiro <iagoribeirospider2007dpr@gmail.com>
2025-08-26 17:03:26 -0300

    feat: implementa sistema de turnos e seleção de área

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 
+import os
+
 quadro = [
     ['-','-','-'],
     ['-','-','-'],
@@ -15,4 +17,15 @@ def exibir_jogo():
         print()
     print()
 
-exibir_jogo()
+def selecionar_area():
+    jogador_atual = 'X' if turno % 2 == 0 else 'O'
+    linha = int(input('Escolha uma linha: '))
+    coluna = int(input('Escolha uma coluna: '))
+    quadro[linha][coluna] = jogador_atual
+
+turno = 0
+for i in range(9):
+    exibir_jogo()
+    selecionar_area()
+    turno+=1
+    os.system('cls')
