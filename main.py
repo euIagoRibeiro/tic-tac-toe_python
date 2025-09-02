494215646f09f1ab1eaae4a5eef429e283723d06
euIagoRibeiro <iagoribeirospider2007dpr@gmail.com>
2025-09-01 23:18:31 -0300

    feat: adiciona lógica para verificar vitória e velha

--- main.py
+++ main.py
@@ -22,10 +22,50 @@ def selecionar_area():
     linha = int(input('Escolha uma linha: '))
     coluna = int(input('Escolha uma coluna: '))
     quadro[linha][coluna] = jogador_atual
+    return jogador_atual
 
 turno = 0
 for i in range(9):
+
     exibir_jogo()
-    selecionar_area()
+    jogador_atual = selecionar_area()
+
+    def vitoria():
+        os.system('cls')
+        exibir_jogo()
+        print(f'O jogador "{jogador_atual}" venceu o jogo')
+
+    def velha():
+        os.system('cls')
+        exibir_jogo()
+        print('O jogo deu velha')
+
+    if quadro[0][0] == jogador_atual and quadro[0][1] == jogador_atual and quadro[0][2] == jogador_atual:
+        vitoria()
+        break
+    elif quadro[1][0] == jogador_atual and quadro[1][1] == jogador_atual and quadro [1][2] == jogador_atual:
+        vitoria()
+        break
+    elif quadro[2][0] == jogador_atual and quadro[2][1] == jogador_atual and quadro[2][2] == jogador_atual:
+        vitoria()
+        break
+    elif quadro[0][0] == jogador_atual and quadro[1][0] == jogador_atual and quadro[2][0] == jogador_atual:
+        vitoria()
+        break
+    elif quadro[0][1] == jogador_atual and quadro[1][1] == jogador_atual and quadro[2][1] == jogador_atual:
+        vitoria()
+        break
+    elif quadro[0][2] == jogador_atual and quadro[1][2] == jogador_atual and quadro[2][2] == jogador_atual:
+        vitoria()
+        break
+    elif quadro[0][0] == jogador_atual and quadro[1][1] == jogador_atual and quadro[2][2] == jogador_atual:
+        vitoria()
+        break
+    elif quadro[0][2] == jogador_atual and quadro[1][1] == jogador_atual and quadro[2][0] == jogador_atual:
+        vitoria()
+        break
+    else:
+        velha()
+   
     turno+=1
     os.system('cls')
